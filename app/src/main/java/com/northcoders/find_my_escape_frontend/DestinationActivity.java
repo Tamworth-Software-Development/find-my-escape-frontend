34cd271b74d1fb8fa0db078c5ea4a4a55a5371e2
Hamdy Abdalla <ha17007001@gmail.com>
2025-01-13 16:45:16 +0000

    feat: create DestinationActivity.java and add Spinner partial implementation

---
+++ app/src/main/java/com/northcoders/find_my_escape_frontend/DestinationActivity.java
@@ -0,0 +1,48 @@
+package com.northcoders.find_my_escape_frontend;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.navigation.NavigationBarView;
+
+public class DestinationActivity extends AppCompatActivity {
+
+    private Spinner dropdown;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destination);
+        dropdown = findViewById(R.id.dropdown);
+
+        // Create an ArrayAdapter using the string array and a default spinner
+        ArrayAdapter<CharSequence> dropdownAdapter = ArrayAdapter.createFromResource(this, R.array.activity_array, android.R.layout.simple_spinner_dropdown_item);
+
+        // Specify the layout to use when the list of choices appears
+        dropdownAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        dropdown.setAdapter(dropdownAdapter);
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+}
