191980ac890e4ed0bd74986d57b585aeee0c0e8d
Mohamed Fraitis <mfraitis1@gmail.com>
2025-01-15 14:53:21 +0000

    feat: redirect to accounts page when logged in

--- app/src/main/java/com/northcoders/find_my_escape_frontend/Signup.java
+++ app/src/main/java/com/northcoders/find_my_escape_frontend/Signup.java
@@ -33,7 +33,7 @@ public class Signup extends AppCompatActivity {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            Intent intent = new Intent(getApplicationContext(), Account.class);
             startActivity(intent);
             finish();
         }
