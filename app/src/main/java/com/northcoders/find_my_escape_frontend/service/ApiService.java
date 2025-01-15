7a5c347e0d7e0d486bd4331e9dd29ce21a15d2c7
Mohamed Fraitis <mfraitis1@gmail.com>
2025-01-15 23:23:37 +0000

    feat: add GET and PUT methods to apiService

--- app/src/main/java/com/northcoders/find_my_escape_frontend/service/ApiService.java
+++ app/src/main/java/com/northcoders/find_my_escape_frontend/service/ApiService.java
@@ -4,12 +4,21 @@ import com.northcoders.find_my_escape_frontend.model.User;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface ApiService {
 
     //todo: check signup api route
     @POST("users/signup")
     Call<Void> signupUser(@Body User user);
+
+    @GET("user/{uid}")
+    Call<User> getUser(@Path("uid") String uid);
+
+    @PUT("user/{uid}")
+    Call<Void> updateUser(@Path("uid") String uid, @Body User user);
 }
 
